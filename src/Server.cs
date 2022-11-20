865362af15aa2625dedc12a1eb8c8fa76539fde3
JuniorWells <kostas.chaniotakis@gmail.com>
2022-11-20 15:47:32 +0100

    second task

--- src/Server.cs
+++ src/Server.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -7,4 +8,12 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+Socket socket = server.AcceptSocket(); // wait for client
+Console.WriteLine("Connection Accepted");
+
+string responseString = "+PONG\r\n";
+
+Byte[] sendBytes = Encoding.ASCII.GetBytes(responseString);
+int i = socket.Send(sendBytes);
+Console.WriteLine($"Message sent /> : {responseString}");
+
