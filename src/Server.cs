1b85f160ea790ea88f0d723adf10051dfa780812
JuniorWells <kostas.chaniotakis@gmail.com>
2022-11-21 13:37:52 +0100

    multiple pings 1st attempt

--- src/Server.cs
+++ src/Server.cs
@@ -2,18 +2,22 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-Socket socket = server.AcceptSocket(); // wait for client
-Console.WriteLine("Connection Accepted");
 
-string responseString = "+PONG\r\n";
+while (true)
+{
+	Socket socket = server.AcceptSocket(); // wait for client
+	Console.WriteLine("Connection Accepted");
 
-Byte[] sendBytes = Encoding.ASCII.GetBytes(responseString);
-int i = socket.Send(sendBytes);
-Console.WriteLine($"Message sent /> : {responseString}");
+	string responseString = "+PONG\r\n";
+
+	Byte[] sendBytes = Encoding.ASCII.GetBytes(responseString);
+	int i = socket.Send(sendBytes);
+	Console.WriteLine($"Bytes sent: {i.ToString()}");
+	Console.WriteLine($"Message sent /> : {responseString}");
+
+}
 
